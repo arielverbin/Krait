a75ebf0379e273d8d50aea887d7bd37b38521c10
Ariel Verbin <arielverbin@icloud.com>
2025-04-08 03:25:59 +0300

    feat: implement If semantics

---
+++ src/semantics/If.hpp
@@ -0,0 +1,23 @@
+#ifndef SEMANTICS_IF_HPP
+#define SEMANTICS_IF_HPP
+
+#include "ASTNode.hpp"
+
+namespace semantics {
+
+class If : public ASTNode {
+public:
+    If(std::shared_ptr<ASTNode> cond, std::shared_ptr<ASTNode> trueBlock, std::shared_ptr<ASTNode> falseBlock);
+
+    virtual std::shared_ptr<core::Object> evaluate(runtime::Environment& state) override;
+
+    virtual ~If() = default;
+
+private:
+    std::shared_ptr<ASTNode> condition_;
+    std::shared_ptr<ASTNode> trueBlock_;
+    std::shared_ptr<ASTNode> falseBlock_;
+};
+}
+
+#endif // SEMANTICS_IF_HPP
